937d34fd4333763619750f2607f6a85f7cb2d91f
Lifan Qian <qianlifan1990@gmail.com>
2018-11-09 14:45:37 -0800

    update base on requirement

--- src/main/java/csye/Assignment/student_info_system/service/GenericServices.java
+++ src/main/java/csye/Assignment/student_info_system/service/GenericServices.java
@@ -1,10 +1,13 @@
 package csye.Assignment.student_info_system.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBDeleteExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBQueryExpression;
 import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBScanExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
 
 import csye.Assignment.student_info_system.datamodel.BasicDynamoObject;
 import csye.Assignment.student_info_system.datamodel.DynamoDBConnector;
@@ -25,14 +28,24 @@ public class GenericServices{
 		return service;
 	}
 	
-	public <T extends BasicDynamoObject> T deleteItem(Class<T> objectClass, long id) {
-		T object = mapper.load(objectClass, id);
+	public <T extends BasicDynamoObject> T deleteItem(T object) {
 		mapper.delete(object, new DynamoDBDeleteExpression());
 		return object;
 	}
 	
-	public <T extends BasicDynamoObject> T getItem(Class<T> objectClass, long id) {
-		return mapper.load(objectClass, id);
+	public <T extends BasicDynamoObject> T getItem(Class<T> objectClass, String objectId, String indexName) {
+		HashMap<String, AttributeValue> eav = new HashMap<String, AttributeValue>();
+		eav.put(":v1",  new AttributeValue().withN(objectId));
+		
+		DynamoDBQueryExpression<T> queryExpression = new DynamoDBQueryExpression<T>()
+			    .withIndexName(indexName)
+			    .withKeyConditionExpression(indexName + " = :v1")
+			    .withConsistentRead(false)
+				.withExpressionAttributeValues(eav);
+
+		List<T> list =  mapper.query(objectClass, queryExpression);
+		if (list.size() == 0) return null;
+		return list.get(0);
 	}
 	
 	public <T extends BasicDynamoObject> List<T> getAllItems(Class<T> objectClass){
